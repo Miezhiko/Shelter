131f60264d685d837ea13d288ea1279cbafbb0b0
Miezhiko <Miezhiko@gmail.com>
2023-08-03 10:47:44 +0400

    fix and optimize libgit2 code
    Signed-off-by: Miezhiko <Miezhiko@gmail.com>

--- src/vcs/git2.hpp
+++ src/vcs/git2.hpp
@@ -106,7 +106,7 @@ template <> void Repo <VCS::Git> :: pull (
     set_hash( local_hash );
   }
 
-  const auto repo_upstream = upstream();
+  const auto& repo_upstream = upstream();
   std::vector<std::string> upstream_split;
   std::stringstream ss(repo_upstream);
   std::string token;
@@ -158,7 +158,8 @@ template <> void Repo <VCS::Git> :: pull (
   }
 
   if (connected) {
-    error = git_remote_fetch(remote, nullptr, nullptr, nullptr);
+    git_fetch_options fetch_opts = GIT_FETCH_OPTIONS_INIT;
+    error = git_remote_fetch(remote, nullptr, &fetch_opts, nullptr);
     if (error != 0) {
       std::cout << "git_remote_fetch error code: " << error << std::endl;
       git_remote_free(remote);
@@ -171,8 +172,14 @@ template <> void Repo <VCS::Git> :: pull (
       upstream_split.size() > 1 ? upstream_split[1]
                                 : branch_name;
 
+    std::string upstream;
+    upstream.reserve(upstream_remote.size() + upstream_branch.size() + 1);
+    upstream.append(upstream_remote);
+    upstream.push_back('/');
+    upstream.append(upstream_branch);
+
     git_reference* branch_ref = nullptr;
-    error = git_branch_lookup(&branch_ref, repo, upstream_branch.c_str(), GIT_BRANCH_REMOTE);
+    error = git_branch_lookup(&branch_ref, repo, upstream.c_str(), GIT_BRANCH_REMOTE);
     if (error != 0) {
       std::cout << "git_branch_lookup error code: " << error << std::endl;
       git_remote_free(remote);
@@ -181,10 +188,10 @@ template <> void Repo <VCS::Git> :: pull (
       return;
     }
 
-    git_object *peeled = nullptr;
-    error = git_reference_peel(&peeled, branch_ref, GIT_OBJECT_COMMIT);
-    if (error != 0 || peeled == nullptr) {
-      std::cout << "git_reference_peel error code: " << error << std::endl;
+    git_annotated_commit* commit = nullptr;
+    error = git_annotated_commit_from_ref(&commit, repo, branch_ref);
+    if (error != 0) {
+      std::cout << "git_annotated_commit_from_ref error code: " << error << std::endl;
       git_reference_free(branch_ref);
       git_remote_free(remote);
       git_repository_free(repo);
@@ -192,18 +199,21 @@ template <> void Repo <VCS::Git> :: pull (
       return;
     }
 
-    if (git_object_type(peeled) != GIT_OBJECT_COMMIT) {
-      std::cout << "Peeled object is not a commit" << std::endl;
+    const git_oid* commit_oid = git_annotated_commit_id(commit);
+
+    git_object* commit_object = nullptr;
+    int error = git_object_lookup(&commit_object, repo, commit_oid, GIT_OBJECT_COMMIT);
+    if (error != 0) {
+      std::cout << "git_object_lookup error code: " << error << std::endl;
       git_reference_free(branch_ref);
       git_remote_free(remote);
       git_repository_free(repo);
       git_libgit2_shutdown();
-      return;
     }
 
     git_checkout_options checkout_opts = GIT_CHECKOUT_OPTIONS_INIT;
-    checkout_opts.checkout_strategy = GIT_CHECKOUT_SAFE;
-    error = git_checkout_tree(repo, peeled, &checkout_opts);
+    checkout_opts.checkout_strategy = GIT_CHECKOUT_FORCE;
+    error = git_checkout_tree(repo, commit_object, &checkout_opts);
     if (error != 0) {
       std::cout << "git_checkout_tree error code: " << error << std::endl;
       git_reference_free(branch_ref);
@@ -213,9 +223,33 @@ template <> void Repo <VCS::Git> :: pull (
       return;
     }
 
-    error = git_branch_set_upstream(branch_ref, upstream_branch.c_str());
+    git_reference* local_branch_ref = nullptr;
+    error = git_branch_lookup(&local_branch_ref, repo, branch_name, GIT_BRANCH_LOCAL);
+    if (error != 0) {
+      std::cout << "git_branch_lookup for local error code: " << error << std::endl;
+      git_reference_free(branch_ref);
+      git_remote_free(remote);
+      git_repository_free(repo);
+      git_libgit2_shutdown();
+    }
+
+    git_reference *new_target_ref;
+    error = git_reference_set_target(&new_target_ref, local_branch_ref, commit_oid, nullptr);
+    if (error != 0) {
+      std::cout << "git_reference_set_target for local error code: " << error << std::endl;
+      git_reference_free(local_branch_ref);
+      git_reference_free(branch_ref);
+      git_remote_free(remote);
+      git_repository_free(repo);
+      git_libgit2_shutdown();
+    }
+
+    git_reference_free(new_target_ref);
+
+    error = git_branch_set_upstream(local_branch_ref, upstream.c_str());
     if (error != 0) {
       std::cout << "git_branch_set_upstream error code: " << error << std::endl;
+      git_reference_free(local_branch_ref);
       git_reference_free(branch_ref);
       git_remote_free(remote);
       git_repository_free(repo);
@@ -223,6 +257,8 @@ template <> void Repo <VCS::Git> :: pull (
       return;
     }
 
+    git_annotated_commit_free(commit);
+    git_reference_free(local_branch_ref);
     git_reference_free(branch_ref);
   } else {
     const auto pull_cmd = "git pull " + repo_upstream;
@@ -231,6 +267,8 @@ template <> void Repo <VCS::Git> :: pull (
       std::cout << output << std::endl;
     }
   }
+
+  git_repository_state_cleanup(repo);
 
   set_hash( remote_hash );
 
@@ -309,7 +347,7 @@ template <> void Repo <VCS::Git> :: rebase (
     set_hash( local_hash );
   }
 
-  const auto repo_upstream = upstream();
+  const auto& repo_upstream = upstream();
   const auto remote_hash = get_remote_hash( repo_upstream );
 
   if (local_hash == remote_hash) {
